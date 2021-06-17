4a8b41799a0650ca15eeae7583196d222af3d8aa
zkobrinsky <zachkobrinsky@gmail.com>
2021-06-17 19:56:36 -0400

    removed comments

--- bargraph2/index.js
+++ bargraph2/index.js
@@ -6,12 +6,6 @@ let svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
 
 makeData(dataset, 20, 30, 900, true);
 let max = Math.max(...dataset);
-console.log(max);
-// window.onresize = () => {
-//   w = document.documentElement.clientWidth;
-//   h = document.documentElement.clientHeight;
-//   svg.attr("width", w).attr("height", h);
-// };
 
 svg
   .selectAll("rect")
@@ -25,11 +19,6 @@ svg
     height: (d, i) => (h / max) * max,
     fill: d => `rgb(${(255 * d) / max}, 0, 0)`,
   });
-//   .attr("x", (d, i) => i * (w / dataset.length))
-//   .attr("y", d => h - d)
-//   .attr("width", w / dataset.length - padding)
-//   .attr("height", (d, i) => (h / max) * max)
-//   .style("fill", d => `rgb(${(255 * d) / max}, 0, 0)`);
 
 function makeData(dataset, length, min, max, sorted = false) {
   for (let i = 0; i < length; i++) {
